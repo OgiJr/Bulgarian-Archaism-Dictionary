4b804e336b043cc426c2253c3ac03fb89fc83ffe
Ognian Trajanov Jr <ogtrjr@gmail.com>
2020-01-23 01:57:16 +0300

    Google OCR v1
    I've created a simple example of text recognition using Google OCR. Bugs that need fixing: wait for user to enter camera permission.

---
+++ ArchaismDictionaryAndroidApp/ArchaismDictionaryAndroidApp/MainActivity.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Android.Gms.Vision;
+using Android.Gms.Vision.Texts;
+using Android.Util;
+using Android.Graphics;
+using Android.Support.V4.App;
+using Android;
+using static Android.Gms.Vision.Detector;
+using System.Text;
+
+namespace ArchaismDictionaryAndroidApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, ISurfaceHolderCallback, IProcessor
+    {
+        private SurfaceView cameraView;
+        private TextView textView;
+        private CameraSource cameraSource;
+        private const int requestCameraPermission = 1001;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            cameraView = FindViewById<SurfaceView>(Resource.Id.surface_view);
+            textView = FindViewById<TextView>(Resource.Id.text_view);
+
+            TextRecognizer textRecognizer = new TextRecognizer.Builder(ApplicationContext).Build();
+
+            if (textRecognizer.IsOperational)
+            {
+                cameraSource = new CameraSource.Builder(ApplicationContext, textRecognizer).
+                    SetFacing(CameraFacing.Back).
+                    SetRequestedPreviewSize(1280, 1024).
+                    SetRequestedFps(2.0f).
+                    SetAutoFocusEnabled(true).Build();
+
+                cameraView.Holder.AddCallback(this);
+                textRecognizer.SetProcessor(this);
+            }
+            else
+            {
+                Log.Error("Main Activity", "Имаше грешка при инициализирането на Google Vision");
+            }
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height)
+        {
+
+        }
+
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            if (ActivityCompat.CheckSelfPermission(ApplicationContext, Manifest.Permission.Camera) != Android.Content.PM.Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, new string[]
+                {
+                Android.Manifest.Permission.Camera
+                }, requestCameraPermission);
+
+                cameraSource.Start(cameraView.Holder);
+            }
+        }
+
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
+            cameraSource.Stop();
+        }
+
+        public void ReceiveDetections(Detections detections)
+        {
+            SparseArray items = detections.DetectedItems;
+            textView.Post(() =>
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                for(int i = 0; i < items.Size(); i++)
+                {
+                    TextBlock item = (TextBlock)items.ValueAt(i);
+                    stringBuilder.Append(item);
+                    stringBuilder.Append("\n");
+                }
+                textView.Text = stringBuilder.ToString();
+            });
+        }
+
+        public void Release()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
