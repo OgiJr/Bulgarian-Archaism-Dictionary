2adf2eb586643b44dcc8a573dfd2f35ecd138112
Yoana Stankova <yoana19st@gmail.com>
2020-02-26 23:58:07 +0200

    major update 2
    PHP migration
    forms
    db connection

---
+++ Website/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <title></title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+    </script>
+  </head>
+
+  <body>
---
+++ Website/index.php
@@ -0,0 +1,54 @@
+<?php
+    require "header.php";
+?>
+
+  <div class="main-wrapper">
+  <header class="header">
+    <div class="logo">
+      <h1>BG Archaism</h1>
+    </div>
+    <div class="feature-content">
+      <h1>Речник на българския архаизъм</h1>
+
+      <a href="#" class="theme-btn btn-animated">Речник</a>
+    </div>
+  </header>
+
+  <div class="menu-icon">
+      <img src="https://img.icons8.com/cotton/2x/menu.png"/>
+   </div>
+
+  <div class="nav-bar">
+    <ul>
+      <li>
+        <a href="index.php">
+          Начална страница
+        </a>
+      </li>
+      <li>
+        <a href="words.php">
+          Думи
+        </a>
+      </li>
+      <li>
+        <a href="dictionary.php">
+          Речник
+        </a>
+      </li>
+      <li>
+        <a href="about.php">
+          За нас
+        </a>
+      </li>
+      <li>
+        <a href="gplink">
+          Приложение
+        </a>
+      </li>
+    </ul>
+  </div>
+  </div>
+
+  <?php
+      require "footer.php";
+  ?>
