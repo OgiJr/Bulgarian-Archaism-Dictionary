bf0de9cc599e7de514187d06873b5e6b8a5b5875
Ognian Trajanov Jr <ogtrjr@gmail.com>
2020-03-25 08:10:49 +0200

    JSON Parse
    This version of the iOS app can get the dictionary from the Internet and use it in order to search words.

--- iOS App/ArchaismDictionaryiOS/ArchaismDictionaryiOS/SecondViewController.swift
+++ iOS App/ArchaismDictionaryiOS/ArchaismDictionaryiOS/SecondViewController.swift
@@ -7,32 +7,40 @@
 //
 
 import UIKit
+import Foundation
 
 class SecondViewController: UIViewController {
 
   var dataBase = [[String]]()
     
-    struct WordData
-    {
-        var id: Int = 0
-        var word: String = ""
-        var synonym: String = ""
-        var definition: String = ""
+    struct Welcome: Codable {
+        let property1: [Property1]
+
+        enum CodingKeys: String, CodingKey {
+            case property1 = "Property1"
+        }
     }
-    
-    struct Table
-    {
-        var type: String = ""
-        var version: String = ""
-        var comment: String = ""
-        var name: String = ""
-        var database: String = ""
-        var data: WordData
+
+    struct Property1: Codable {
+        let type: String
+        let version, comment, name, database: String?
+        let data: [Datum]?
     }
-    
-    struct JSONClass
-    {
-        var Property1: Table
+
+    struct Datum: Codable {
+        let the0, the1: String
+        let the2: JSONNull?
+        let the3, id, word: String
+        let synonym: JSONNull?
+        let definition: String
+
+        enum CodingKeys: String, CodingKey {
+            case the0 = "0"
+            case the1 = "1"
+            case the2 = "2"
+            case the3 = "3"
+            case id, word, synonym, definition
+        }
     }
     
     override func viewDidLoad() {
@@ -49,11 +57,38 @@ class SecondViewController: UIViewController {
         }
         do {
             let jsonString = try String(contentsOf: url)
-            print(jsonString)
+            let data = Data(jsonString.utf8)
+            let decoder = JSONDecoder()
+            let dataParsed = try decoder.decode(Welcome.self, from: data)
+            print(dataParsed.property1[2].data![0].word ?? "<no word>")
         }
         catch let error as NSError{
             print("Error: \(error)")
         }
     }
+}
 
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
 }
