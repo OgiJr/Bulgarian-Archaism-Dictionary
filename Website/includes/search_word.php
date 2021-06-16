ded7b27ece8839a6f006f424143da5afbcd5e722
Ognian Trajanov Jr <ogtrjr@gmail.com>
2021-06-16 12:27:09 +0300

    New Website Update1

--- Website/includes/search_word.php
+++ Website/includes/search_word.php
@@ -1,3 +1,3 @@
 <?php
-$con = new PDO("mysql:host=localhost:3306;dbname=archaism_dictionary", "archaism_app", "DictionaryOfArchaism123");
+$con = new PDO("mysql:host=localhost:3306;dbname=archqetw_archaism_dictionary", "admin", "Trajanovisa4");
 ?>
